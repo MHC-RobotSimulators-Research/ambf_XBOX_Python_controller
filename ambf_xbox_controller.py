e050f83c59ecead171bb162d21aea41a6f8253dc
nataliechalfant <natchalfant@gmail.com>
2023-06-21 16:46:00 -0400

    change to inputs library for getting xbox controller values

--- ambf_xbox_controller.py
+++ ambf_xbox_controller.py
@@ -1,193 +1,117 @@
-import pygame
+import inputs
+from inputs import get_gamepad
+from inputs import devices
+import math
+import threading
 
 
-class ambf_xbox_controller:
+class XboxController(object):
     """
-    Creates a controller object using the pygame joystick module and contains methods to return
-    the values of the axes and buttons, developed for use with the Raven II robot.
-
-    Authors: nataliechalfant
+    Gets the state of xbox controller inputs using the inputs library. Modified from
+    https://stackoverflow.com/questions/46506850/how-can-i-get-input-from-an-xbox-one-controller-in-python
     """
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
 
     def __init__(self):
-        pygame.init()
-        # Initialize joystick
-        pygame.joystick.init()
+        self.gamepad = devices.gamepads[0]
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
 
-        if pygame.joystick.get_count() > 0:
-            # Create joystick object
-            pygame.joystick.Joystick(0).init()
-        else:
-            print("No controller found")
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
 
-        # specify controller dead zone for the sticks
-        self.deadzone = 0.08
+    def read(self): # return the buttons/triggers that you care about in this methode
+        lx = self.LeftJoystickX
+        ly = self.LeftJoystickY
+        lt = self.LeftTrigger
+        lb = self.LeftBumper
 
-    def get_axes(self):
-        """
-        Gets all axes values as a list of floats
-        Returns:
-            A list containing the value of each axes as a float between -1 and 1.
-            0 = lj_x
-            1 = lj_y
-            2 = lt
-            3 = rj_x
-            4 = rj_y
-            5 = rt
-        """
+        rx = self.RightJoystickX
+        ry = self.RightJoystickY
+        rt = self.RightTrigger
+        rb = self.RightBumper
+        return [[lx, ly, lt, lb], [rx, ry, rt, rb],
+                [self.A, self.B, self.X, self.Y, self.Back, self.Start]]
 
-        axes = []
-        for i in range(pygame.joystick.Joystick(0).get_numaxes()):
-            axes.append(pygame.joystick.Joystick(0).get_axis(i))
+    def rumble(self, left, right, time):
+        try:
+            self.gamepad.set_vibration(left, right, time)
+        except OSError:
+            print("no space left on device")
 
-        return axes
+    def _monitor_controller(self):
+        while True:
+            try:
+                events = get_gamepad()
+            except inputs.UnknownEventCode:
+                print("unknown event")
 
-    def get_lj_x(self):
-        """
-        Gets the left joystick's x value
-        Returns:
-            The value of the x-axis as a float, -1 to 1. Will return 0
-            unless the value is greater than the dead zone parameter.
-        """
-        if abs(pygame.joystick.Joystick(0).get_axis(0)) > self.deadzone:
-            return pygame.joystick.Joystick(0).get_axis(0)
-        else:
-            return 0
-
-    def get_lj_y(self):
-        """
-        Gets the left joystick's y value
-        Returns:
-            The value of the y-axis as a float, -1 to 1. Will return 0
-            unless the value is greater than the dead zone parameter.
-        """
-        if abs(pygame.joystick.Joystick(0).get_axis(1)) > self.deadzone:
-            return pygame.joystick.Joystick(0).get_axis(1)
-        else:
-            return 0
-
-    def get_lt(self):
-        """
-        Gets the left trigger's value
-        Returns:
-            The value of the left trigger as a float, -1 to 1.
-        """
-        return pygame.joystick.Joystick(0).get_axis(2)
-
-    def get_rj_x(self):
-        """
-        Gets the right joystick's x value
-        Returns:
-            The value of the x-axis as a float, -1 to 1. Will return 0
-            unless the value is greater than the dead zone parameter.
-        """
-        if abs(pygame.joystick.Joystick(0).get_axis(3)) > self.deadzone:
-            return pygame.joystick.Joystick(0).get_axis(3)
-        else:
-            return 0
-
-    def get_rj_y(self):
-        """
-        Gets the right joystick's y value
-        Returns:
-            The value of the y-axis as a float, -1 to 1. Will return 0
-            unless the value is greater than the dead zone parameter.
-        """
-        if abs(pygame.joystick.Joystick(0).get_axis(4)) > self.deadzone:
-            return pygame.joystick.Joystick(0).get_axis(4)
-        else:
-            return 0
-
-    def get_rt(self):
-        """
-        Gets the left trigger's value
-        Returns:
-            The value of the left trigger as a float, -1 to 1.
-        """
-        return pygame.joystick.Joystick(0).get_axis(5)
-
-    def get_buttons_bool(self):
-        """
-        Gets the state of all the controller's buttons
-        Returns:
-            A list containing the state af all buttons as booleans, True for pressed and false otherwise
-            0 = A
-            1 = B
-            2 = X
-            3 = Y
-            4 = LB
-            5 = RB
-            6 = Back
-            7 = Start
-            8 = ???
-            9 = LS
-            10 = RS
-        """
-        buttons = []
-        for i in range(pygame.joystick.Joystick(0).get_numbuttons()):
-            if pygame.joystick.Joystick(0).get_button(i) == 1:
-                buttons.append(True)
-            else:
-                buttons.append(False)
-
-        return buttons
-
-    def get_buttons(self):
-        """
-        Gets the state of all the controller buttons
-        Returns:
-            A list containing the state af all buttons as integers, 1 for pressed and 0 otherwise
-            0 = A
-            1 = B
-            2 = X
-            3 = Y
-            4 = LB
-            5 = RB
-            6 = Back
-            7 = Start
-            8 = ???
-            9 = LS
-            10 = RS
-        """
-        buttons = []
-        for i in range(pygame.joystick.Joystick(0).get_numbuttons()):
-            buttons.append(pygame.joystick.Joystick(0).get_button(i))
-
-        return buttons
-
-    def get_dpad(self):
-        """
-        Gets the state of the dpad buttons
-        Returns:
-            A list of ints, when
-            [0] = -1 -> left
-            [0] = 1 -> right
-            [1] = -1 -> down
-            [1] = 1 -> up
-        """
-        return pygame.joystick.Joystick(0).get_hat(0)
-
-    def rumble(self):
-        pygame.event.get()
-
-        print(pygame.joystick.Joystick(0).rumble(0.5, 0.5, 0))
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.Y = event.state #previously switched with X
+                elif event.code == 'BTN_WEST':
+                    self.X = event.state #previously switched with Y
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY1':
+                    self.LeftDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY2':
+                    self.RightDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY3':
+                    self.UpDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY4':
+                    self.DownDPad = event.state
 
 
-# Stuff for testing
-def main():
+if __name__ == '__main__':
+    joy = XboxController()
+    joy.rumble(1, 1, 100)
 
-    pygame.joystick.init()
-    # Initialize the pygame joystick module followed by the joystick with the id 1
-    controller = ambf_xbox_controller()
-    clock = pygame.time.Clock()
-
-    running = True
-
-    while running:
-        pygame.event.get()
-        # if controller.get_buttons_bool()[0]:
-        controller.rumble()
-        print("trigger ", controller.get_lt())
-
-if __name__ == "__main__":
-    main()
+    while True:
+        print(joy.read())
